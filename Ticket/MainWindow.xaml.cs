4f28ba316a81d91e9dccf066bbe0deac64b75538
OmarTagliabue <sfomaa@gmail.com>
2025-08-07 12:57:53 +0200

    seconde modifiche ancora non funziona, ma forse ce la facciamo

--- Ticket/MainWindow.xaml.cs
+++ Ticket/MainWindow.xaml.cs
@@ -143,7 +143,6 @@ namespace TicketingApp
             win.Show();
         }
 
-
         private void ThemeToggleButton_Checked(object sender, RoutedEventArgs e)
         {
             SetBaseTheme(true);
